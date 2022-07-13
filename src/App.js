f880ae561d9af6756c3f559d687904d94df1749c
leseraphina <leseraphina@gmail.com>
2022-07-13 17:48:40 +0900

    react-color

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import './App.css'
 
-function App() {
+const selectColor = ['red','blue','green','purple','brown']
+function ProImg(props){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <img src={`./data/${props.color}.jpg`} alt="상세이미지" />
+  )
 }
 
-export default App;
+function ProColor(props){
+  function optionColor(){
+    let colors = selectColor;
+    return colors.map( (item, index) => {
+      return (<option value={item} key={index}>{item}</option>)
+    })
+  }
+  function onColorChange(evt){
+    // console.log(evt.target.value)
+    props.hColorChange(evt.target.value)
+  }
+  return (
+    <p>
+      <label htmlFor="color">color:</label>
+      <select 
+        id="color"
+        defaultValue={props.color}
+        onChange={onColorChange}
+        >
+        {optionColor()}
+      </select>
+    </p>
+  )
+}
+
+function App(){
+  const [color,setColor] = useState('blue')
+  function hColorChange(selectColor){
+    setColor(selectColor)
+  }
+  return (
+    <div className="main">
+    <ProImg color={color} />
+    <ProColor 
+      color={color}
+      hColorChange={hColorChange} />
+    </div>
+  )
+}
+
+export default App
